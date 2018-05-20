8396e9ddb4656bda32cdd01e1ac50302a529333b
chocolateboy <chocolate@cpan.org>
2018-05-20 01:01:41 +0100

    code cleanup

--- src/async-any.js
+++ src/async-any.js
@@ -3,7 +3,8 @@ import isPromise           from 'is-promise'
 import observableToPromise from 'observable-to-promise'
 import once                from 'onetime'
 
-function handlePromise (promise, done) {
+// pipe the promise's result/error to the callback
+function pipePromise (promise, done) {
     const onFulfilled = result => { done(null, result) }
     const onRejected = error => {
         // ensure a promise rejected with a falsey value isn't
@@ -15,12 +16,12 @@ function handlePromise (promise, done) {
     promise.then(onFulfilled, onRejected)
 }
 
-function asyncAny (callback, continuation) {
+function asyncAny (task, callback) {
     let promise
 
-    if (!continuation) {
+    if (!callback) {
         promise = new Promise((resolve, reject) => {
-            continuation = function (error, result) {
+            callback = function (error, result) {
                 if (error) {
                     reject(error)
                 } else {
@@ -30,20 +31,21 @@ function asyncAny (callback, continuation) {
         })
     }
 
-    const done = once(continuation)
-    const result = callback(done)
+    const done = once(callback)
+    let result = task(done)
 
     // prefer promise if the result is both a promise and an observable:
     // https://github.com/gulpjs/async-done/issues/36
-    // https://github.com/avajs/ava/blob/e4446544f075c6613bae030f285b23181ddff1af/lib/assert.js#L345-L351
-    if (isPromise(result)) {
-        handlePromise(result, done)
-    } // eslint-disable-line brace-style
-
+    // https://github.com/avajs/ava/blob/v1.0.0-beta.4/lib/assert.js#L345-L351
+    //
     // XXX isObservable is currently buggy with RxJS@6 observables
     // https://github.com/sindresorhus/is-observable/issues/1#issuecomment-387843191
-    else if (isObservable(result)) {
-        handlePromise(observableToPromise(result), done)
+    if (isObservable(result) && !isPromise(result)) {
+        result = observableToPromise(result)
+    }
+
+    if (isPromise(result)) {
+        pipePromise(result, done)
     }
 
     return promise
